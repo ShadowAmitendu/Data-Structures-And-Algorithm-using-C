4b2df8e1813d96f55e97c3dd9760cfaf04ca11e8
Amitendu Bikash Dhusiya <amitendubikashdhusiya@gmail.com>
2024-11-05 20:12:34 +0530

    resend

---
+++ sLL - EleminateDuplicate.c
@@ -0,0 +1,110 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for the Singly Linked List node
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+// Function to create a new node
+struct Node *createNode(int data)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(struct Node **head, int data)
+{
+    struct Node *newNode = createNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        struct Node *temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Function to display the list
+void displayList(struct Node *head)
+{
+    struct Node *temp = head;
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to eliminate duplicate elements
+void eliminateDuplicates(struct Node *head)
+{
+    struct Node *current = head;
+    struct Node *index = NULL;
+    struct Node *temp = NULL;
+
+    while (current != NULL && current->next != NULL)
+    {
+        temp = current; // temp is used to track the previous node
+        index = current->next;
+        while (index != NULL)
+        {
+            if (current->data == index->data)
+            {
+                temp->next = index->next; // Skip the duplicate node
+                free(index);              // Free the memory of the duplicate node
+                index = temp->next;
+            }
+            else
+            {
+                temp = index;
+                index = index->next;
+            }
+        }
+        current = current->next;
+    }
+}
+
+// Main function to demonstrate the program
+int main()
+{
+    struct Node *head = NULL;
+    int n, data;
+
+    printf("Enter the number of elements to insert in the list: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+    }
+
+    printf("Original List: ");
+    displayList(head);
+
+    eliminateDuplicates(head);
+
+    printf("List after eliminating duplicates: ");
+    displayList(head);
+
+    return 0;
+}
---
+++ sLL - SoringinASC.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    node *next;
+};
+
+typedef struct Node node;
+
+// Function to create a new node
+node *createNode(int data)
+{
+    node *newNode = (node *)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertAtEnd(node **head, int data)
+{
+    node *newNode = createNode(data);
+    if (*head == NULL)
+    {
+        *head = newNode;
+    }
+    else
+    {
+        node *temp = *head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Function to display the list
+void displayList(node *head)
+{
+    node *temp = head;
+    if (head == NULL)
+    {
+        printf("List is empty\n");
+        return;
+    }
+    while (temp != NULL)    
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+void sortList(node **head)
+{
+    if (*head == NULL)
+        return;
+
+    int swapped;
+    node *ptr1;
+    node *lptr = NULL;
+
+    do
+    {
+        swapped = 0;
+        ptr1 = *head;
+
+        while (ptr1->next != lptr)
+        {
+            if (ptr1->data > ptr1->next->data)
+            {
+                int temp = ptr1->data;
+                ptr1->data = ptr1->next->data;
+                ptr1->next->data = temp;
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
+
+// Main function to demonstrate the program
+int main()
+{
+    node *head = NULL;
+    int n, data;
+
+    printf("Enter the number of elements to insert in the list: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter element %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+    }
+
+    printf("Original List: ");
+    displayList(head);
+
+    sortList(&head);
+
+    printf("Sorted List: ");
+    displayList(head);
+
+    return 0;
+}
